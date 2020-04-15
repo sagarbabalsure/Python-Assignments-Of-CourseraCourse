82cbb15dd593926aa80ee578e00ebfd3b40919f4
sagarbabalsure <sagarbabalsure@gmail.com>
2020-04-15 09:41:46 +0530

    new assignments added

---
+++ Python Data Structures/assignment-9.4(week5).py
@@ -0,0 +1,22 @@
+'''9.4 Write a program to read through the mbox-short.txt and figure out who has sent the greatest number of mail messages. The program looks for 'From ' lines and takes the second word of those lines as the person who sent the mail. The program creates a Python dictionary that maps the sender's mail address to a count of the number of times they appear in the file. After the dictionary is produced, the program reads through the dictionary using a maximum loop to find the most prolific committer.
+'''
+
+name = input("Enter file:")
+if len(name) < 1 : name = "mbox-short.txt"
+handle = open(name)
+mails = list()
+count = dict()
+for line in handle:
+    if line.startswith('From: '):
+        mails.append(line.rstrip().split(" ")[1])
+for mail in mails:
+    count[mail]=count.get(mail,0)+1
+m = max(count.values())
+print(count.keys()[count.values().index(m)],m)
+
+'''OUTPUT
+
+Enter file:mbox-short.txt
+cwen@iupui.edu 5
+
+'''
